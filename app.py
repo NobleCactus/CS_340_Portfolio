009c3222cfca897730e9474bb521c16b320721a3
Andrew <andrew.eppinger@yahoo.com>
2021-01-30 12:32:45 -0500

    Takes out test dictionary. Begin work on portfolio project.

--- app.py
+++ app.py
@@ -1,32 +1,6 @@
 from flask import Flask, render_template
 import os
 
-people_from_app_py =[
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
 
 # Configuration
 
@@ -37,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("main.j2")
 
 
 # Listener
