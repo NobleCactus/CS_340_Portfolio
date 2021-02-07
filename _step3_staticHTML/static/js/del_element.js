849efc8f3190fd9a9c9b961af106b7d071ca55ba
joshluo123 <luojo@oregonstate.edu>
2021-02-07 14:12:34 -0500

    hide search results in delete/update until search button is clicked

--- _step3_staticHTML/static/js/del_element.js
+++ _step3_staticHTML/static/js/del_element.js
@@ -1,52 +1,58 @@
 document.addEventListener("DOMContentLoaded", bindButtons);
 
 function bindButtons() {
-	document.getElementById("selTitleSearch").addEventListener("click", function(event){
-		document.getElementById("delDevSearch").style.display = "none";
-		document.getElementById("delPlatSearch").style.display = "none";
-		document.getElementById("delFranchiseSearch").style.display = "none";
+  document.getElementById("selTitleSearch").addEventListener("click", function(event){
+    document.getElementById("delDevSearch").style.display = "none";
+    document.getElementById("delPlatSearch").style.display = "none";
+    document.getElementById("delFranchiseSearch").style.display = "none";
 
-		document.getElementById("delTitleSearch").style.display = "block";
+    document.getElementById("delTitleSearch").style.display = "block";
 
-		document.getElementById("delSuccessful").style.display = "none"
-	});
+    document.getElementById("delSuccessful").style.display = "none"
+  });
 
-	document.getElementById("selDevSearch").addEventListener("click", function(event){
-		document.getElementById("delTitleSearch").style.display = "none";
-		document.getElementById("delPlatSearch").style.display = "none";
-		document.getElementById("delFranchiseSearch").style.display = "none";
+  document.getElementById("selDevSearch").addEventListener("click", function(event){
+    document.getElementById("delTitleSearch").style.display = "none";
+    document.getElementById("delPlatSearch").style.display = "none";
+    document.getElementById("delFranchiseSearch").style.display = "none";
 
-		document.getElementById("delDevSearch").style.display = "block";
+    document.getElementById("delDevSearch").style.display = "block";
 
-		document.getElementById("delSuccessful").style.display = "none"
-	});
+    document.getElementById("delSuccessful").style.display = "none"
+  });
 
-	document.getElementById("selPlatSearch").addEventListener("click", function(event){
-		document.getElementById("delTitleSearch").style.display = "none";
-		document.getElementById("delDevSearch").style.display = "none";
-		document.getElementById("delFranchiseSearch").style.display = "none";
+  document.getElementById("selPlatSearch").addEventListener("click", function(event){
+    document.getElementById("delTitleSearch").style.display = "none";
+    document.getElementById("delDevSearch").style.display = "none";
+    document.getElementById("delFranchiseSearch").style.display = "none";
 
-		document.getElementById("delPlatSearch").style.display = "block";
+    document.getElementById("delPlatSearch").style.display = "block";
 
-		document.getElementById("delSuccessful").style.display = "none"
-	});
+    document.getElementById("delSuccessful").style.display = "none"
+  });
 
-	document.getElementById("selFranchiseSearch").addEventListener("click", function(event){
-		document.getElementById("delTitleSearch").style.display = "none";
-		document.getElementById("delDevSearch").style.display = "none";
-		document.getElementById("delPlatSearch").style.display = "none";
+  document.getElementById("selFranchiseSearch").addEventListener("click", function(event){
+    document.getElementById("delTitleSearch").style.display = "none";
+    document.getElementById("delDevSearch").style.display = "none";
+    document.getElementById("delPlatSearch").style.display = "none";
 
-		document.getElementById("delFranchiseSearch").style.display = "block";
+    document.getElementById("delFranchiseSearch").style.display = "block";
 
-		document.getElementById("delSuccessful").style.display = "none"
-	});
+    document.getElementById("delSuccessful").style.display = "none"
+  });
 
-	Array.from(document.getElementsByClassName("delButton")).forEach(function(element) {
-		element.addEventListener("click", function(event) {
-			document.getElementById("delSuccessful").style.display = "block";
-			setTimeout(function() {
-				document.getElementById("delSuccessful").style.display = "none"
-			}, 1500);
-		})
-	});
+  Array.from(document.getElementsByClassName("delButton")).forEach(function(element) {
+    element.addEventListener("click", function(event) {
+      document.getElementById("delSuccessful").style.display = "block";
+      setTimeout(function() {
+        document.getElementById("delSuccessful").style.display = "none"
+      }, 1500);
+    })
+  });
+
+  Array.from(document.getElementsByClassName("searchButton")).forEach(function(element) {
+    element.addEventListener("click", function(event) {
+      document.getElementById("searchResults").style.display = "block"
+    })
+  });
 }
