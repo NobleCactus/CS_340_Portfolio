7d83c736bd7d576e61d71d26077d020b6ab381ec
joshluo123 <luojo@oregonstate.edu>
2021-02-21 18:04:26 -0500

    added required notifications in html and not null field verification in js files.

--- static/js/add_element.js
+++ static/js/add_element.js
@@ -42,79 +42,283 @@ function bindButtons() {
 	});
 
 	document.getElementById("addTitleButton").addEventListener('click', function(event){
-		var req = new XMLHttpRequest();
-		
+		// get an array of checked platforms
 		var title_plat = new Array();
 		var i = 0;
-		Array.from(document.getElementsByClassName("addTitlePlat")).forEach(function(element) {
-			if (element.checked){
-				title_plat[i] = element.value
-				i++;
+			Array.from(document.getElementsByClassName("addTitlePlat")).forEach(function(element) {
+				if (element.checked){
+					title_plat[i] = element.value
+					i++;
+				}
+			});
+		
+		// verify not NULL parameters have values
+		if (document.getElementById('addTitleName').value == "" ||
+			document.getElementById('addTitleDate').value == "" ||
+			document.getElementById('addTitleDev').value == "" ||
+			title_plat.length == 0) {
+			if (document.getElementById('addTitleName').value == "") {
+				document.getElementById('noName').style.display = "inline";
 			}
-		});
+			if (document.getElementById('addTitleDate').value == "") {
+				document.getElementById('noDate').style.display = "inline";
+			}
+			if (document.getElementById('addTitleDev').value == "") {
+				document.getElementById('noDev').style.display = "inline";
+			}
+			if (title_plat.length == 0) {
+				document.getElementById('noPlat').style.display = "inline";
+			}
+		} else {
+			var req = new XMLHttpRequest();
 
-		var payload = {"action": "addTitle",
-						"titleName": document.getElementById('addTitleName').value,
-						"titlePlat": title_plat,
-						"titleRelease": document.getElementById('addTitleDate').value, 
-						"Genre": document.getElementById('addTitleGenre').value, 
-						"Franchise": document.getElementById('addTitleFranchise').value,
-						"Developer": document.getElementById('addTitleDev').value,
-						"ESRB": document.getElementById('titleESRB').value};
+			var payload = {"action": "addTitle",
+							"titleName": document.getElementById('addTitleName').value,
+							"titlePlat": title_plat,
+							"titleRelease": document.getElementById('addTitleDate').value, 
+							"titleGenre": document.getElementById('addTitleGenre').value, 
+							"titleFranchise": document.getElementById('addTitleFranchise').value,
+							"titleDev": document.getElementById('addTitleDev').value,
+							"titleESRB": document.getElementById('titleESRB').value};
 
-		req.open('POST', '/add', false);
-		req.setRequestHeader('Content-Type', 'application/json');
-		req.send(JSON.stringify(payload));
+			req.open('POST', '/add', true);
+			req.setRequestHeader('Content-Type', 'application/json');
+
+			req.addEventListener('load', function(){
+				if (req.status >= 200 && req.status < 400) {
+					// POST request successful, check response for query status
+
+					// if query successful
+						/* show add successful message and delayed disappear
+						
+						document.getElementById("addSuccessful").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addSuccessful").style.display = "none"
+						}, 1500);
+
+						*/
+
+					// if query unsuccessful
+						/* show add failed message and delayed disappear
+						
+						document.getElementById("addFailed").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addFailed").style.display = "none"
+						}, 1500);
+
+						*/
+				} else {
+					console.log("Error in network request: " + req.statusText);
+			}});
+			req.send(JSON.stringify(payload));
+
+			document.getElementById('noTitleName').style.display = "none";
+			document.getElementById('noTitleDate').style.display = "none";
+			document.getElementById('noTitleDev').style.display = "none";
+			document.getElementById('noTitlePlat').style.display = "none";
+		}
 	});
 
 	document.getElementById("addDevButon").addEventListener('click', function(event){
-		var req = new XMLHttpRequest();
+		// verify not NULL parameters have values
+		if (document.getElementById('addDevName').value == "" ||
+			document.getElementById('addDevCountry').value == "" ||
+			document.getElementById('addDevDate').value == "") {
+			if (document.getElementById('addDevName').value == "") {
+				document.getElementById('noDevName').style.display = "inline";
+			}
+			if (document.getElementById('addDevCountry').value == "") {
+				document.getElementById('noDevCountry').style.display = "inline";
+			}
+			if (document.getElementById('addDevDate').value == "") {
+				document.getElementById('noDevDate').style.display = "inline";
+			}
+		} else {
+			var req = new XMLHttpRequest();
 
-		var payload = {"action": "addDev",
-						"devName": document.getElementById('addDevName').value,
-						"devCountry": document.getElementById('addDevCountry').value,
-						"devDate": document.getElementById('addDevDate').value};
+			var payload = {"action": "addDev",
+							"devName": document.getElementById('addDevName').value,
+							"devCountry": document.getElementById('addDevCountry').value,
+							"devDate": document.getElementById('addDevDate').value};
 
-		req.open('POST', '/add', false);
-		req.setRequestHeader('Content-Type', 'application/json');
-		req.send(JSON.stringify(payload));
+			req.open('POST', '/add', true);
+			req.setRequestHeader('Content-Type', 'application/json');
+
+			req.addEventListener('load', function(){
+				if (req.status >= 200 && req.status < 400) {
+					// POST request successful, check response for query status
+
+					// if query successful
+						/* show add successful message and delayed disappear
+						
+						document.getElementById("addSuccessful").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addSuccessful").style.display = "none"
+						}, 1500);
+
+						*/
+
+					// if query unsuccessful
+						/* show add failed message and delayed disappear
+						
+						document.getElementById("addFailed").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addFailed").style.display = "none"
+						}, 1500);
+
+						*/
+				} else {
+					console.log("Error in network request: " + req.statusText);
+			}});
+
+			req.send(JSON.stringify(payload));
+
+			document.getElementById('noDevName').style.display = "none";
+			document.getElementById('noDevCountry').style.display = "none";
+			document.getElementById('noDevDate').style.display = "none";
+		}
 	});
 
 	document.getElementById("addPlatButton").addEventListener('click', function(event){
-		var req = new XMLHttpRequest();
-
+		// verify not NULL parameters have values
 		var inProdCheck = Array.from(document.getElementsByClassName("addPlatInProd"));
-		if (inProdCheck[0].checked) {
-			inProdCheck = 1;
-		} else if (inProdCheck[1].checked) {
-			inProdCheck = 0;
+			if (inProdCheck[0].checked) {
+				inProdCheck = "Yes";
+			} else if (inProdCheck[1].checked) {
+				inProdCheck = "No";
+			} else {
+				inProdCheck = "";
+			}
+
+		if (document.getElementById('addPlatName').value == "" ||
+			document.getElementById('addPlatDate').value == "" ||
+			document.getElementById('addPlatDev').value == "" ||
+			inProdCheck == "") {
+			if (document.getElementById('addPlatName').value == "") {
+				console.log("FOUND AN EMPTY PARAMETER");
+				document.getElementById('noPlatName').style.display = "inline";
+			}
+			if (document.getElementById('addPlatDate').value == "") {
+				document.getElementById('noPlatDate').style.display = "inline";
+			}
+			if (document.getElementById('addPlatDev').value == "") {
+				document.getElementById('noPlatDev').style.display = "inline";
+			}
+			if (inProdCheck == "") {
+				document.getElementById('noPlatInProd').style.display = "inline";
+			}
 		} else {
-			inProdCheck = "";
+			var req = new XMLHttpRequest();
+
+			var inProdCheck = Array.from(document.getElementsByClassName("addPlatInProd"));
+			if (inProdCheck[0].checked) {
+				inProdCheck = 1;
+			} else if (inProdCheck[1].checked) {
+				inProdCheck = 0;
+			} else {
+				inProdCheck = "";
+			}
+
+			var payload = {"action": "addPlat",
+							"platName": document.getElementById('addPlatName').value,
+							"platDate": document.getElementById('addPlatDate').value,
+							"platDev": document.getElementById('addPlatDev').value,
+							"platInProd": inProdCheck};
+
+			req.open('POST', '/add', true);
+			req.setRequestHeader('Content-Type', 'application/json');
+
+			req.addEventListener('load', function(){
+				if (req.status >= 200 && req.status < 400) {
+					// POST request successful, check response for query status
+
+					// if query successful
+						/* show add successful message and delayed disappear
+						
+						document.getElementById("addSuccessful").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addSuccessful").style.display = "none"
+						}, 1500);
+
+						*/
+
+					// if query unsuccessful
+						/* show add failed message and delayed disappear
+						
+						document.getElementById("addFailed").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addFailed").style.display = "none"
+						}, 1500);
+
+						*/
+				} else {
+					console.log("Error in network request: " + req.statusText);
+			}});
+
+			req.send(JSON.stringify(payload));
+
+			document.getElementById('noPlatName').style.display = "none";
+			document.getElementById('noPlatDate').style.display = "none";
+			document.getElementById('noPlatDev').style.display = "none";
+			document.getElementById('noPlatInProd').style.display = "none";
 		}
-
-		var payload = {"action": "addPlat",
-						"platName": document.getElementById('addPlatName').value,
-						"platDate": document.getElementById('addPlatDate').value,
-						"platDev": document.getElementById('addPlatDev').value,
-						"platInProd": inProdCheck};
-
-		req.open('POST', '/add', false);
-		req.setRequestHeader('Content-Type', 'application/json');
-		req.send(JSON.stringify(payload));
 	});
 
 	document.getElementById("addFranchiseButton").addEventListener('click', function(event){
-		var req = new XMLHttpRequest();
-		
-		var payload = {"action": "addFranchise",
-						"franchiseName": document.getElementById('addFranchiseName').value,
-						"franchiseDev": document.getElementById('addFranchiseDev').value};
+		// verify not NULL parameters have values
+		if (document.getElementById('addFranchiseName').value == "" ||
+			document.getElementById('addFranchiseDev').value == "") {
+			if (document.getElementById('addFranchiseName').value == "") {
+				document.getElementById('noFranchiseName').style.display = "inline";
+			}
+			if (document.getElementById('addFranchiseDev').value == "") {
+				document.getElementById('noFranchiseDev').style.display = "inline";
+			}
+		} else {
+			var req = new XMLHttpRequest();
+			
+			var payload = {"action": "addFranchise",
+							"franchiseName": document.getElementById('addFranchiseName').value,
+							"franchiseDev": document.getElementById('addFranchiseDev').value};
 
-		req.open('POST', '/add', false);
-		req.setRequestHeader('Content-Type', 'application/json');
-		req.send(JSON.stringify(payload));
+			req.open('POST', '/add', true);
+			req.setRequestHeader('Content-Type', 'application/json');
+
+			req.addEventListener('load', function(){
+				if (req.status >= 200 && req.status < 400) {
+					// POST request successful, check response for query status
+
+					// if query successful
+						/* show add successful message and delayed disappear
+						
+						document.getElementById("addSuccessful").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addSuccessful").style.display = "none"
+						}, 1500);
+
+						*/
+
+					// if query unsuccessful
+						/* show add failed message and delayed disappear
+						
+						document.getElementById("addFailed").style.display = "block";
+						setTimeout(function() {
+							document.getElementById("addFailed").style.display = "none"
+						}, 1500);
+
+						*/
+				} else {
+					console.log("Error in network request: " + req.statusText);
+			}});
+
+			req.send(JSON.stringify(payload));
+
+			document.getElementById('noFranchiseName').style.display = "none";
+			document.getElementById('noFranchiseDev').style.display = "none";
+		}
 	});
 
+	// delete this once we get db queries working
 	Array.from(document.getElementsByClassName("addButton")).forEach(function(element) {
 		element.addEventListener("click", function(event) {
 			document.getElementById("addSuccessful").style.display = "block";
--- static/js/main.js
+++ static/js/main.js
@@ -2,19 +2,36 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 
 function bindButtons() {
 	document.getElementByid("filterButton").addEventListener('click', function(event){
+		// verify not NULL parameters have values
+
 		var req = new XMLHttpRequest();
 		
 		var payload = {"titleSearch": document.getElementById('titleSearch').value,
 						"filterPlatform": document.getElementById('filterPlatform').value, 
 						"fromDate": document.getElementById('fromDate').value, 
 						"toDate": document.getElementById('toDate').value, 
-						"Genre": document.getElementById('filterGenre').value, 
-						"Franchise": document.getElementById('filterFranchise').value,
-						"Developer": document.getElementById('filterDev').value,
-						"ESRB": document.getElementById('filterESRB').value};
+						"genre": document.getElementById('filterGenre').value, 
+						"franchise": document.getElementById('filterFranchise').value,
+						"developer": document.getElementById('filterDev').value,
+						"esrb": document.getElementById('filterESRB').value};
 
-		req.open('POST', '/', false);
+		req.open('POST', '/', true);
 		req.setRequestHeader('Content-Type', 'application/json');
+
+		req.addEventListener('load', function(){
+			if (req.status >= 200 && req.status < 400) {
+				// POST request successful, check response for query status
+
+				// if query successful,
+					// use DOM to dynamically add query result table to webpage
+					// reset filter fields?
+
+				// if query unsuccessful (shouldn't happen if filter parameters are verified first)
+
+			} else {
+				console.log("Error in network request: " + req.statusText);
+		}});
+
 		req.send(JSON.stringify(payload));
 	});
 }
