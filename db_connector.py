804ff6b4c3301c3011890eb4bd879ef8f526811f
Andrew Eppinger <eppingea@flip1.engr.oregonstate.edu>
2021-02-22 15:37:30 -0800

    took caps off of pymysql

--- db_connector.py
+++ db_connector.py
@@ -1,4 +1,4 @@
-import PyMySQL as mariadb
+import pymysql as mariadb
 from db_credentials import host, user, passwd, db
 
 def connect_to_database(host = host, user = user, passwd = passwd, db = db):
