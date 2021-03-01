6eb39d384ca760b773417b9107b89b59cf8b6ce8
joshluo123 <luojo@oregonstate.edu>
2021-03-01 13:26:38 -0500

    Update update_element.js

--- static/js/update_element.js
+++ static/js/update_element.js
@@ -471,8 +471,7 @@ function bind_update_buttons() {
       // name cell_elements[0]
       update_name = document.createElement('input');
       update_name.type = "text";
-      console.log(row_element.childNodes[0].textContent);
-      update_name.value = row_element.childNodes[0].value;
+      update_name.value = row_element.childNodes[0].textContent;
       row_element.replaceChild(update_name, cell_elements[0]);
 
       // platform list cell_elements[1]
