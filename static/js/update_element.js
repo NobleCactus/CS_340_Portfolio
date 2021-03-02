9ad7f0705de1e4b2977cebfb0aadaa1066450653
joshluo123 <luojo@oregonstate.edu>
2021-03-02 15:58:15 -0500

    Update update_element.js

--- static/js/update_element.js
+++ static/js/update_element.js
@@ -445,6 +445,10 @@ function bind_updateTitle_buttons() {
         td_cell = document.createElement('td');
         td_cell.style.textAlign = "left";
 
+        console.log(plat_elements);
+        console.log(plat_elements[0]);
+        console.log(plat_elements[0][0]);
+
           //*** platform names isn't showing on webpage, but it is in the HTML ***
         var plat_option = document.createElement("input");
         plat_option.setAttribute("type", "checkbox");
