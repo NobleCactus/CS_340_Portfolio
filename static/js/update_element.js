fedbc5c7bd75e3ff5919bb958bc4119e61b3070d
joshluo123 <luojo@oregonstate.edu>
2021-03-02 01:18:39 -0500

    Update update_element.js

--- static/js/update_element.js
+++ static/js/update_element.js
@@ -778,8 +778,8 @@ function bind_updatePlat_buttons() {
           // set default selection to original value
         if (cell_elements[2].textContent != "") {
           var index = 1;
-          console.log(update_inProd.childNodes[index].value);
-          while (cell_elements[2].textContent != update_inProd.childNodes[index].value) {
+          console.log(update_inProd.childNodes[index]);
+          while (cell_elements[2].textContent != update_inProd.childNodes[index].textContent) {
             index++;
           }
           update_inProd.childNodes[index].selected = true;
