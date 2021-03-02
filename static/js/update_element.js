c6679297785e6be26461218ad71d28d9ea984874
joshluo123 <luojo@oregonstate.edu>
2021-03-02 00:15:18 -0500

    Update update_element.js

--- static/js/update_element.js
+++ static/js/update_element.js
@@ -431,13 +431,14 @@ function bind_update_buttons() {
         td_cell.appendChild(plat_option);
 
         for (var i = 1; i < plat_elements.length; i++) {
-          //break_tag = document.createElement("br");
-          //td_cell.appendChild(break_tag);
+          break_tag = document.createElement("br");
+          td_cell.appendChild(break_tag);
 
           plat_option = document.createElement("input");
           plat_option.setAttribute("type", "checkbox");
           plat_option.setAttribute("value", plat_elements[i][0]);
           plat_option.textContent = plat_elements[i][1];
+          plat_option.setAttribute("checked", true);
           td_cell.appendChild(plat_option);
         }
           // set default selections to original values
