cc9605dc04682319e7167c53add4dc3c9ed5719f
joshluo123 <luojo@oregonstate.edu>
2021-03-02 00:23:16 -0500

    Update update_element.js

--- static/js/update_element.js
+++ static/js/update_element.js
@@ -424,15 +424,12 @@ function bind_update_buttons() {
         td_cell = document.createElement('td');
         td_cell.style.textAlign = "left";
 
+        //*** text isn't showing on webpage
         plat_option = document.createElement("input");
         plat_option.setAttribute("type", "checkbox");
-        console.log(plat_elements[0][1]);
+        plat_option.setAttribute("value", plat_elements[0][0]);
         plat_option.textContent = plat_elements[0][1];
-        //plat_option.setAttribute("value", plat_elements[0][0]);
-        
         td_cell.appendChild(plat_option);
-
-        /*
         for (var i = 1; i < plat_elements.length; i++) {
           break_tag = document.createElement("br");
           td_cell.appendChild(break_tag);
@@ -442,9 +439,10 @@ function bind_update_buttons() {
           plat_option.setAttribute("value", plat_elements[i][0]);
           plat_option.textContent = plat_elements[i][1];
           td_cell.appendChild(plat_option);
-        }*/
+        }
           // set default selections to original values
         //plat_option.setAttribute("checked", true);
+        console.log(cell_elements[1]);
 
         row_element.replaceChild(td_cell, cell_elements[1]);
 
