c28cc624bf5946cb11387258eeb994567af95c74
joshluo123 <luojo@oregonstate.edu>
2021-03-09 14:24:05 -0500

    Update update_element.js

--- static/js/update_element.js
+++ static/js/update_element.js
@@ -100,6 +100,7 @@ function bindButtons() {
           plat_list = document.createElement('ul');
           plat_list.setAttribute('class', 'platformList');
           for (var j = 0; j < res[i][7].length; j++) {
+            console.log(res[i][7][j]);
             plat_item = document.createElement('li');
             plat_item.textContent = res[i][7][j];
             plat_list.appendChild(plat_item);
