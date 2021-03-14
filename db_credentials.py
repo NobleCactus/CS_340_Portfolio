3bd6e082ccc08f300cff63f6455b42438f066971
Andrew <andrew.eppinger@yahoo.com>
2021-03-14 13:22:49 -0400

    added host and db back into db_credentials

--- db_credentials.py
+++ db_credentials.py
@@ -4,7 +4,7 @@
 # the following will be used by the db_connector.py and 
 # in turn by all the Python code in this codebase to interact with the database
 
-host = ''  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
 user = ''
 passwd = ''
-db = ''
+db = 'cs340_eppingea'
