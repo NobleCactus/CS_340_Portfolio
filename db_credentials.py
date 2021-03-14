ac2f0d69ad649213c9315fad492af27b4ea00cac
Andrew <andrew.eppinger@yahoo.com>
2021-03-14 13:19:14 -0400

    Removed actual db credentials

--- db_credentials.py
+++ db_credentials.py
@@ -4,7 +4,7 @@
 # the following will be used by the db_connector.py and 
 # in turn by all the Python code in this codebase to interact with the database
 
-host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = 'cs340_eppingea'
-passwd = 'Oregonstate2022'
-db = 'cs340_eppingea'
+host = ''  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = ''
+passwd = ''
+db = ''
